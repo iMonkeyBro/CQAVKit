8faf2a97fb5c72de0a64ea166c20b177ecff05a8
LiuChaoqun <chaoqunmail@126.com>
2022-02-18 02:26:02 +0800

    继续写编码

--- CQAVKit/CQCoder/CQVideoEncoder.m
+++ CQAVKit/CQCoder/CQVideoEncoder.m
@@ -26,7 +26,7 @@
 - (instancetype)initWithConfig:(CQVideoCoderConfig *)config {
     if (self = [super init]) {
         _config = config;
-        
+        [self initVideoToolBox];
     }
     return self;
 }
@@ -42,7 +42,23 @@
 
 #pragma mark - Public Func
 - (void)videoEncodeWithSampleBuffer:(CMSampleBufferRef)sampleBuffer {
-    
+    CFRetain(sampleBuffer);
+    dispatch_async(_encodeQueue, ^{
+        // 帧数据
+        CVImageBufferRef imageBuffer = (CVImageBufferRef)CMSampleBufferGetImageBuffer(sampleBuffer);
+        // 该帧的时间戳
+        _frameID ++;
+        CMTime timeStamp = CMTimeMake(_frameID, 1000);
+        // 持续时间
+        CMTime duration = kCMTimeInvalid;
+        // 编码
+        VTEncodeInfoFlags flags;
+        OSStatus status = VTCompressionSessionEncodeFrame(_encodeSession, imageBuffer, timeStamp, duration, NULL, NULL, &flags);
+        if (status != noErr) {
+            NSLog(@"CQVideoEncoder-VTCompressionSessionEncodeFrame failed. status = %d", (int)status);
+        }
+        CFRelease(sampleBuffer);
+    });
 }
 
 #pragma mark - VideoToolBox
@@ -67,21 +83,63 @@
         return;
     }
     // 设置编码器属性
-    // 设置实时执行
+    // 设置实时编码
     status = VTSessionSetProperty(_encodeSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
     NSLog(@"CQVideoEncoder-VTSessionSetProperty set RealTime. return status = %d", (int)status);
-    // 指定编码比特流的配置文件和级别。直播一般使用baseline，可减少由B帧带来的延时
+    // 指定编码比特流的配置文件和级别。直播一般使用baseline，抛弃B帧，可减少由B帧带来的延时
     status = VTSessionSetProperty(_encodeSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Baseline_AutoLevel);
     NSLog(@"CQVideoEncoder-VTSessionSetProperty set ProfileLevel. return status = %d", (int)status);
+    // 设置码率均值(比特率可以高于此。默认比特率为0，表示视频编码器。应该确定压缩数据的大小。注意，比特率设置只在定时时有效)
+    CFNumberRef bit = (__bridge CFNumberRef)@(_config.bitrate);
+    status = VTSessionSetProperty(_encodeSession, kVTCompressionPropertyKey_AverageBitRate, bit);
+    NSLog(@"CQVideoEncoder-VTSessionSetProperty set AverageBitRate. return status = %d", (int)status);
+    // 码率限制(只在定时时起作用)*待确认
+    CFArrayRef limits = (__bridge CFArrayRef)@[@(_config.bitrate / 4), @(_config.bitrate * 4)];
+    status = VTSessionSetProperty(_encodeSession, kVTCompressionPropertyKey_DataRateLimits,limits);
+    NSLog(@"CQVideoEncoder-VTSessionSetProperty set DataRateLimits. return status = %d", (int)status);
+    //设置关键帧间隔(GOPSize)GOP太大图像会模糊
+    CFNumberRef maxKeyFrameInterval = (__bridge CFNumberRef)@(_config.fps * 2);
+    status = VTSessionSetProperty(_encodeSession, kVTCompressionPropertyKey_MaxKeyFrameInterval, maxKeyFrameInterval);
+    NSLog(@"CQVideoEncoder-VTSessionSetProperty set MaxKeyFrameInterval. return status = %d", (int)status);
+    //设置fps(预期)
+    CFNumberRef expectedFrameRate = (__bridge CFNumberRef)@(_config.fps);
+    status = VTSessionSetProperty(_encodeSession, kVTCompressionPropertyKey_ExpectedFrameRate, expectedFrameRate);
+    NSLog(@"CQVideoEncoder-VTSessionSetProperty set ExpectedFrameRate. return status = %d", (int)status);
     
+    //准备编码
+    status = VTCompressionSessionPrepareToEncodeFrames(_encodeSession);
+    if (status != noErr) {
+        NSLog(@"CQVideoEncoder-VTCompressionSessionPrepareToEncodeFrames failed. status = %d", (int)status);
+        return;
+    }
 }
 
 
 
-
 #pragma mark - 编码完成回调
+// startCode 长度 4
+const Byte startCode[] = "\x00\x00\x00\x01";
 void videoEncoderCallBack(void *outputCallbackRefCon, void *sourceFrameRefCon, OSStatus status, VTEncodeInfoFlags infoFlags, CMSampleBufferRef sampleBuffer) {
-    
+    if (status != noErr) {
+        // 有错误
+        NSLog(@"CQVideoEncoder-VideoEncodeCallback: encode error, status = %d", (int)status);
+        return;
+    }
+    if (!CMSampleBufferDataIsReady(sampleBuffer)) {
+        // 数据没有准备好
+        NSLog(@"CQVideoEncoder-VideoEncodeCallback: data is not ready");
+        return;
+    }
+    // 拿到自己
+    CQVideoEncoder *encoder = (__bridge CQVideoEncoder *)outputCallbackRefCon;
+    // 判断是否是关键帧
+    BOOL isKeyFrame = NO;
+    CFArrayRef attachArr = CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, true);
+    isKeyFrame = !CFDictionaryContainsKey(CFArrayGetValueAtIndex(attachArr, 0), kCMSampleAttachmentKey_NotSync);
+    // 获取sps pps数据，只需要获取一次，保存在h264文件头即可
+    if (isKeyFrame && !encoder->_isHasSpsPps) {
+        
+    }
 }
 
 #pragma mark - Load
